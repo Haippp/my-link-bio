628df803f9b3f5ce3360d7912c664a6f35eb0c95
Haippp <haiperdesain@gmail.com>
2026-07-21 09:34:04 +0800

    style: penambahan animasi ketik dan timbul

--- app/page.js
+++ app/page.js
@@ -25,13 +25,10 @@ function MyProfile() {
   useEffect(() => {
     const interval = setInterval(() => {
       setStatus((prevStatus) => {
-        console.log(prevStatus)
         let newStatus = ""
         if(prevStatus == "text-[#79c0ff]") newStatus = "text-[#fffd79]"
         else if(prevStatus == "text-[#fffd79]") newStatus = "text-[#7ee787]"
         else {newStatus = "text-[#79c0ff]"}
-
-        console.log(newStatus)
         return newStatus
       })
     }, 500)
@@ -42,7 +39,7 @@ function MyProfile() {
           <img src="/my-pixelart.png" alt="pixel-art" style={{height: "230px"}} className="px-7"/>
           <div className="flex-1 min-w-0 text-xs md:text-base">
             <p className="text-[#5c6773] text-center md:text-left"><AnimateASCII/>-----------------------------<AnimateASCII/></p>
-            <div className="grid grid-cols-[110px_1fr] gap-y-1.5">
+            <div className="grid grid-cols-[110px_1fr] gap-y-1.5 mt-3">
               <span className="text-[#79c0ff]">Nama</span>
               <span className="text-[#e6edf3]">: Haiper Alexander</span>
               <span className="text-[#79c0ff]">Peran</span>
@@ -69,12 +66,36 @@ function MyProfile() {
   );
 }
 
+function Typing({value}){
+  const [text, setText] = useState("")
+  const [isType, setIsType] = useState(true)
+
+  useEffect(()=>{
+    let i = 0
+    const timer = setInterval(() => {
+      if (i < value.length) {
+        const char = value[i]
+        console.log(value[i], i)
+        setText(prevText => prevText + char)
+        i++
+      } else {
+        setIsType(false)
+        clearInterval(timer)
+      }
+    }, 150)
+
+    return () => clearInterval(timer)
+  }, [value])
+
+  return <span className="text-white">{text}{isType && <span className="animate-pulse">|</span>}</span>
+}
+
 function Command({ user = "guest", command }) {
   return (
     <div className="mt-2">
       <span className="text-blue-400">{user}@link-bio</span>
       <span>:~$ </span>
-      <span className="text-white">{command}</span>
+      <Typing value={command}/>
     </div>
   );
 }
@@ -85,9 +106,9 @@ export default function Home() {
       <MyProfile/>
       <Command command="ls -la /links"/>
       <div className="mt-2">
-        <a href="https://github.com/Haippp" className="hover:bg-green-700 text-white inline-block w-full px-4 py-"><span className="text-green-400">1.</span> Github</a>
-        <a href="http://linkedin.com/in/hipni-c030324071/" className="hover:bg-green-700 text-white inline-block w-full px-4 py-"><span className="text-green-400">2.</span> LinkedIn</a>
-        <a href="https://www.instagram.com/haiper_alexander/" className="hover:bg-green-700 text-white inline-block w-full px-4 py-"><span className="text-green-400">3.</span> Instagram</a>
+        <a href="https://github.com/Haippp" className="fade1 hover:bg-green-700 text-white inline-block w-full px-4 py-"><span className="text-green-400">1.</span> Github</a>
+        <a href="http://linkedin.com/in/hipni-c030324071/" className="fade2 hover:bg-green-700 text-white inline-block w-full px-4 py-"><span className="text-green-400">2.</span> LinkedIn</a>
+        <a href="https://www.instagram.com/haiper_alexander/" className="fade3 hover:bg-green-700 text-white inline-block w-full px-4 py-"><span className="text-green-400">3.</span> Instagram</a>
       </div>
     </div>
   );
